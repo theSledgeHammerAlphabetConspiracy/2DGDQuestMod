73fc3902fc8d702dda1aaa4e9e56e831171673bf
theSledgeHammerAlphabetConspiracy <to8myZ@gmail.com>
2023-02-05 20:37:27 -0600

    added basic mobile controls which suck

---
+++ 04-24-finite-state-machine changed/my scripts/Demo.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_TouchScreenButton_pressed():
+	$Node2D/mobile._set_current(true)
+	$Node2D/Camera2D._set_current(false)
